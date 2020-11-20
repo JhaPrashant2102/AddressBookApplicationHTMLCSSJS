cbb081655cf57fc0dd2b9213555da24753a2fcaf
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-11-20 19:14:34 +0530

    added feature to populate the address book from the local storage

--- js/home.js
+++ js/home.js
@@ -1,33 +1,36 @@
+let contactDetailsList;
 window.addEventListener('DOMContentLoaded',(event)=>{
+    //contactDetailsList = createContactsJSON();
+    contactDetailsList = getContactDetailsFromLocalStorage();
+    document.querySelector(".contact-count").textContent = contactDetailsList.length;
     createInnerHtml();
-  });
+});
   
-  const createInnerHtml = () => {
-      const headerHtml = "<th></th><th>Fullname</th><th>Address</th><th>City</th>"
-                          +"<th>State</th><th>Zip Code</th><th>Phone Number</th>";
-      let innerHtml = `${headerHtml}`;
-      let addressBookList = createContactsJSON();
-      for(const contact of addressBookList){
-        innerHtml = `${innerHtml}
-      <tr><td></td>
-      <td>${contact._name}</td>
-      <td>${contact._address}</td>
-      <td>${contact._city}</td>
-      <td>${contact._state}</td>
-      <td>${contact._zip}</td>
-      <td>${contact._phone}</td>
-      <td>
-          <img id="1" onclick="remove(this)" alt="delete"
-              src="../assets/icons/delete-black-18dp.svg">
-          <img id="1" alt="edit" onclick="update(this)" 
-              src="../assets/icons/create-black-18dp.svg">
-      </td>
-      </tr>
-      `;
-      }
-      document.querySelector('#table-display').innerHTML = innerHtml;
-  }
-  const createContactsJSON = () => {
+const createInnerHtml = () => {
+    const headerHtml = "<th></th><th>Fullname</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th>";
+    let innerHtml = `${headerHtml}`;
+    //let contactList = createContactsJSON();
+    for(const contact of contactDetailsList){
+    innerHtml = `${innerHtml}
+    <tr><td></td>
+    <td>${contact._name}</td>
+    <td>${contact._address}</td>
+    <td>${contact._city}</td>
+    <td>${contact._state}</td>
+    <td>${contact._zip}</td>
+    <td>${contact._phone}</td>
+    <td>
+    <img id="1" onclick="remove(this)" alt="delete"
+        src="../assets/icons/delete-black-18dp.svg">
+    <img id="1" alt="edit" onclick="update(this)" 
+        src="../assets/icons/create-black-18dp.svg">
+    </td>
+    </tr>
+    `;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+const createContactsJSON = () => {
     let contactList = [
         {
             _name: "Mark",
@@ -47,4 +50,8 @@ window.addEventListener('DOMContentLoaded',(event)=>{
       }
     ];
     return contactList;
+}
+const getContactDetailsFromLocalStorage = () => {
+    return localStorage.getItem('ContactList')?
+                                JSON.parse(localStorage.getItem('ContactList')):[];
   }
